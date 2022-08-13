651e56050a15d57d3940a2ea3f7376fbc5738587
israela <israela.zimru@tipranks.com>
2022-08-13 14:29:11 +0300

    connecting all end to end

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
@@ -18,6 +19,51 @@ export const createPost = async (req, res) => {
     await newPost.save(); //tring to save asny new document in DB
 
     res.status(201).json(newPost); //success creating new document
+  } catch (e) {
+    res.status(400).json({ message: e.message });
+  }
+};
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(_id))
+      return res.status(404).send("No post with that id");
+
+    const updatePost = await PostMessage.findByIdAndUpdate(_id, post, {
+      new: true,
+    });
+    res.status(200).json(updatePost); //success creating new document
+  } catch (e) {
+    res.status(400).json({ message: e.message });
+  }
+};
+
+export const deletePost = async (req, res) => {
+  const { id: _id } = req.params;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(_id))
+      return res.status(404).send("No post with that id");
+
+    await PostMessage.findByIdAndDelete(_id);
+    res.status(200).json({ message: "Post deleted successfully" }); //success deleted
+  } catch (e) {
+    res.status(400).json({ message: e.message });
+  }
+};
+
+export const likePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+
+  post.likeCount = post.likeCount + 1;
+  console.log("post", post.likeCount);
+  try {
+    const updatePost = await PostMessage.findByIdAndUpdate(_id, post, {
+      new: true,
+    });
+    res.status(200).json(updatePost);
   } catch (e) {
     res.status(400).json({ message: e.message });
   }
